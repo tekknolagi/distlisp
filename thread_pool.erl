fa2daeff890192a35f6860b723b0a1bf65fa7286
Max Bernstein <tekknolagi@gmail.com>
2016-11-04 11:08:20 -0400

    Add multi-node support to thread pool

--- thread_pool.erl
+++ thread_pool.erl
@@ -1,9 +1,16 @@
 -module(thread_pool).
--export([create/1, next_node/1, add/1, add/2]).
+-export([create/1, create/2, next_node/1, add/1, add/2]).
 
-create(0) -> queue:new();
-create(NumNodes) when NumNodes > 0 ->
-    queue:in(spawn(fun waitforwork/0), create(NumNodes-1)).
+create(0, _ServerPool, t) -> erlang:error(invalid_num_node);
+create(1, _ServerPool, t) -> queue:in(spawn(fun waitforwork/0), queue:new());
+create(NumNodes, ServerPool, t) when NumNodes > 0 ->
+    {NextServer, NewPool} = next_node(ServerPool),
+    Pid = spawn(NextServer, ?MODULE, waitforwork, []),
+    queue:in(Pid, create(NumNodes-1, NewPool)).
+create(NumNodes, ServerPool) ->
+    create(NumNodes, queue:from_list([node()|ServerPool]), t).
+create(NumNodes) ->
+    create(NumNodes, queue:from_list([node()])).
 
 next_node(Nodes) ->
     {{value, FirstNode}, RestNodes} = queue:out(Nodes),
