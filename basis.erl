5e3225ef5a1f9a4770998288fc4e955a52c141a3
Maxwell Bernstein <tekknolagi@gmail.com>
2016-11-13 17:38:47 -0500

    Beautify check-expect prim

--- basis.erl
+++ basis.erl
@@ -103,10 +103,10 @@ worker_proc([NodeExp], Env) -> {NodeName, _} = eval:evalexp(NodeExp, Env),
                                {NewWorkers,
                                 eval:bind(workers, NewWorkers, Env)}.
 
-check_expect([A, B], Env) -> {{bool, Eq}, _} =
-                                 eval:evalexp({list, [{sym, '='}, A, B]}, Env),
-                             if
-                                 Eq   -> io:format("check-expect passed~n", []);
-                                 true -> io:format("check-expect failed~n", [])
-                             end,
-                             {{sym, ok}, Env}.
+check_expect([A, B], Env) ->
+    {{bool, AreEq}, _} = eval:evalexp({list, [{sym, '='}, A, B]}, Env),
+    if
+        AreEq -> io:format("check-expect passed~n", []);
+        true  -> io:format("check-expect failed~n", [])
+    end,
+    {{sym, ok}, Env}.
