8902f08405e7b21c7956268780450d90174330eb
Maxwell Bernstein <tekknolagi@gmail.com>
2016-11-19 02:56:06 -0500

    Shrink BIFs

--- basis.erl
+++ basis.erl
@@ -6,6 +6,7 @@
          print_proc/2, compile_proc/2, env_proc/2, class_proc/2, gcd/2,
          remove_prims/1, bif/1]).
 
+
 basis() ->
     Defs = reader:read_program(file, "basis.lisp"),
     lists:foldl(fun (Cur, Env) ->
@@ -13,27 +14,27 @@ basis() ->
                         NewEnv
                 end, [
                       {'rat', {prim, fun basis:rat_proc/2}},
-                      {'+', {prim, basis:binop(fun erlang:'+'/2, int)}},
-                      {'+r', {prim, fun plusrat_proc/2}},
-                      {'-', {prim, basis:binop(fun erlang:'-'/2, int)}},
-                      {'*', {prim, basis:binop(fun erlang:'*'/2, int)}},
-                      {'/', {prim, basis:binop(fun basis:intdiv/2, int)}},
-                      {'=', {prim, basis:binop(fun erlang:'=:='/2, bool)}},
-                      {'exp', {prim, fun basis:exp_proc/2}},
-                      {'<', {prim, basis:binop(fun erlang:'<'/2, bool)}},
-                      {'>', {prim, basis:binop(fun erlang:'>'/2, bool)}},
-                      {'not', {prim, fun basis:not_proc/2}},
+                      {'+', basis:bif(basis:binop(fun erlang:'+'/2, int))},
+                      {'+r', basis:bif(fun plusrat_proc/2)},
+                      {'-', basis:bif(basis:binop(fun erlang:'-'/2, int))},
+                      {'*', basis:bif(basis:binop(fun erlang:'*'/2, int))},
+                      {'/', basis:bif(basis:binop(fun basis:intdiv/2, int))},
+                      {'=', basis:bif(basis:binop(fun erlang:'=:='/2, bool))},
+                      {'exp', basis:bif(fun basis:exp_proc/2)},
+                      {'<', basis:bif(basis:binop(fun erlang:'<'/2, bool))},
+                      {'>', basis:bif(basis:binop(fun erlang:'>'/2, bool))},
+                      {'not', basis:bif(fun basis:not_proc/2)},
                       {'and', {prim, fun basis:and_proc/2}},
                       {'or', {prim, fun basis:or_proc/2}},
-                      {'cons', {prim, fun basis:cons_proc/2}},
-                      {'car', {prim, fun basis:car_proc/2}},
-                      {'cdr', {prim, fun basis:cdr_proc/2}},
+                      {'cons', basis:bif(fun basis:cons_proc/2)},
+                      {'car', basis:bif(fun basis:car_proc/2)},
+                      {'cdr', basis:bif(fun basis:cdr_proc/2)},
                       {'workers', {list, [{sym, node()}]}},
-                      {'worker', {prim, fun basis:worker_proc/2}},
+                      {'worker', basis:bif(fun basis:worker_proc/2)},
                       {'check-expect', {prim, fun basis:check_expect/2}},
                       {'save-state', {prim, fun basis:save_state/2}},
                       {'load-state', {prim, fun basis:load_state/2}},
-                      {'print', {prim, fun basis:print_proc/2}},
+                      {'print', basis:bif(fun basis:print_proc/2)},
                       {'c', {prim, fun basis:compile_proc/2}},
                       {'env', {prim, fun basis:env_proc/2}},
                       {'ok', {sym, ok}},
@@ -41,44 +42,47 @@ basis() ->
                      ],
                 Defs).
 
+
 binop(F, RT) ->
-    fun ([A, B], Env) ->
-            {{_, AV}, _} = eval:evalexp(A, Env),
-            {{_, BV}, _} = eval:evalexp(B, Env),
+    fun ([{_, AV}, {_, BV}], Env) ->
             {{RT, F(AV, BV)}, Env}
     end.
 
+
 bif(F) ->
-    fun (Args, Env) ->
+    {prim, fun (Args, Env) ->
             Vals = lists:map(fun (Exp) ->
                                      {V, _} = eval:evalexp(Exp, Env),
                                      V
                              end, Args),
             F(Vals, Env)
-    end.
+    end}.
+
 
 rat_proc([N, D], Env) -> {{rat, N, D}, Env}.
+
 
 gcd(A, 0) -> A;
 gcd(A, B) -> basis:gcd(B, A rem B).
 
+
 intdiv(A, B) -> trunc(A/B).
+
 
 simplify(N, D) ->
     Gcd = basis:gcd(N, D),
     {rat, {int, basis:intdiv(N, Gcd)}, {int, basis:intdiv(D, Gcd)}}.
 
-plusrat_proc([A, B], Env) ->
-    {{rat, {int, AN}, {int, AD}}, _} = eval:evalexp(A, Env),
-    {{rat, {int, BN}, {int, BD}}, _} = eval:evalexp(B, Env),
+
+plusrat_proc([{rat, {int, AN}, {int, AD}}, {rat, {int, BN}, {int, BD}}], Env) ->
     {simplify(AN*BD+AD*BN, AD*BD), Env}.
 
-exp_proc([A, B], Env) -> {{int, AV}, _} = eval:evalexp(A, Env),
-                         {{int, BV}, _} = eval:evalexp(B, Env),
-                         {{int, round(math:pow(AV, BV))}, Env}.
 
-not_proc([A], Env) -> {{bool, V}, _} = eval:evalexp(A, Env),
-                      {{bool, not V}, Env}.
+exp_proc([{int, AV}, {int, BV}], Env) -> {{int, round(math:pow(AV, BV))}, Env}.
+
+
+not_proc([{bool, AV}], Env) -> {{bool, not AV}, Env}.
+
 
 and_proc([A, B], Env) -> {{bool, AV}, _} = eval:evalexp(A, Env),
                          if
@@ -86,27 +90,29 @@ and_proc([A, B], Env) -> {{bool, AV}, _} = eval:evalexp(A, Env),
                              true        -> eval:evalexp(B, Env)
                          end.
 
+
 or_proc([A, B], Env) -> {{bool, AV}, _} = eval:evalexp(A, Env),
                         if
                             AV == true -> {{bool, true}, Env};
                             true       -> eval:evalexp(B, Env)
                         end.
 
-cons_proc([X, XS], Env) -> {XV, _} = eval:evalexp(X, Env),
-                           {{list, XSV}, _} = eval:evalexp(XS, Env),
-                           {{list, [XV|XSV]}, Env}.
 
-car_proc([L], Env) -> {{list, [H|_T]}, _} = eval:evalexp(L, Env),
-                      {H, Env}.
+cons_proc([X, {list, XSV}], Env) -> {{list, [X|XSV]}, Env}.
 
-cdr_proc([L], Env) -> {{list, [_H|T]}, _} = eval:evalexp(L, Env),
-                      {{list, T}, Env}.
+
+car_proc([{list, [H|_T]}], Env) -> {H, Env}.
+
+
+cdr_proc([{list, [_H|T]}], Env) -> {{list, T}, Env}.
+
 
 worker_proc([NodeExp], Env) -> {NodeName, _} = eval:evalexp(NodeExp, Env),
                                {list, Workers} = eval:lookup(workers, Env),
                                NewWorkers = {list, [NodeName|Workers]},
                                {NewWorkers,
                                 eval:bind(workers, NewWorkers, Env)}.
+
 
 check_expect([A, B], Env) ->
     {{bool, AreEq}, _} = eval:evalexp({list, [{sym, '='}, A, B]}, Env),
@@ -157,12 +163,14 @@ compile_proc([Name], Env) -> {{sym, NameVal}, _} = eval:evalexp(Name, Env),
                              code:load_file(NameVal),
                              throw(code_reload).
 
+
 env_proc([], Env) ->
     EnvNoPrims = basis:remove_prims(Env),
     EnvWithSyms = lists:map(fun ({Name, V}) ->
                                     {list, [{sym, Name}, V]}
                             end, EnvNoPrims),
     {{list, EnvWithSyms}, Env}.
+
 
 class_proc([Name, Parent], Env) ->
     {{list, [Name, Parent]}, Env}.
