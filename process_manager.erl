dd5b84df7ae913bd9fc28827d796a70c530fbe75
Maxwell Bernstein <tekknolagi@gmail.com>
2016-11-21 09:00:45 -0500

    Add health check callback to process_manager

--- process_manager.erl
+++ process_manager.erl
@@ -4,10 +4,15 @@
 
 isdead(Pid) -> process_info(Pid) =:= undefined.
 
+
 loop(MachineManager) ->
     io:format("DEBUG: Waiting to receive query.~n"),
     receive
         {which_died, Procs} ->
             MachineManager ! {dead_procs, lists:filter(fun isdead/1, Procs)},
+            spawn(fun () ->
+                          timer:sleep(1000),
+                          MachineManager ! check_who_died
+                  end),
             loop(MachineManager)
     end.
