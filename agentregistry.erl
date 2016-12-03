509cd3ebdb42167e11aca5078031bdcb42967097
Maxwell Bernstein <tekknolagi@gmail.com>
2016-12-03 12:39:59 -0500

    Make agent registry

---
+++ agentregistry.erl
@@ -0,0 +1,14 @@
+-module(agentregistry).
+-export([loop/1]).
+
+% http://stackoverflow.com/questions/8817171
+shuffle(Ls) ->
+    [X||{_,X} <- lists:sort([ {random:uniform(), N} || N <- Ls])].
+
+loop(Agents) ->
+    receive
+        {where_are_the_others, RequesterAgent} ->
+            ExceptRequester = Agents -- [RequesterAgent],
+            RequesterAgent ! {other_agents, shuffle(ExceptRequester)},
+            loop(Agents)
+    end.
