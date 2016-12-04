cceb72eb272135e6a685dc7902282772f9ae6a54
Maxwell Bernstein <tekknolagi@gmail.com>
2016-12-04 18:08:57 -0500

    Fix dining philosophers problem

--- stealingworker.erl
+++ stealingworker.erl
@@ -27,16 +27,8 @@ agent_loop() ->
             agent_loop(Worker, Master, WorkQueue, queue:new())
     end.
 
-checkforwork(OtherAgent) ->
-    OtherAgent ! {steal_yo_work, self()},
-    receive
-        nothing_yet -> nothing_yet;
-        {work, _Master, _Pkt}=Work -> Work
-    after % There may be a point in the future where random bits of work are
-          % lost. If that is the case, blame THIS piece of shit. I can't solve
-          % dining philosophers, so I just implemented a timeout. Sue me.
-        500 -> nothing_yet
-    end.
+checkforwork(OtherAgent, MyWorker) ->
+    OtherAgent ! {steal_yo_work, MyWorker}.
 
 agent_loop(Worker, Master, WorkQueue, OtherAgents) ->
     receive
@@ -46,32 +38,24 @@ agent_loop(Worker, Master, WorkQueue, OtherAgents) ->
         {delegate, Id, Exp, Env} ->
             NewWorkQueue = queue:in({work, Master, {Id, Exp, Env}}, WorkQueue),
             agent_loop(Worker, Master, NewWorkQueue, OtherAgents);
-        {steal_yo_work, Requester} ->
+        {steal_yo_work, WorkerFromAgent} ->
             case queue:out(WorkQueue) of
                 {empty, WorkQueue} ->
-                    Requester ! nothing_yet,
+                    WorkerFromAgent ! nothing_yet,
                     agent_loop(Worker, Master, WorkQueue, OtherAgents);
                 {{value, NextItem}, NewWorkQueue} ->
-                    Requester ! NextItem,
+                    WorkerFromAgent ! NextItem,
                     agent_loop(Worker, Master, NewWorkQueue, OtherAgents)
             end;
         {request_for_work, Requester} ->
             case queue:out(WorkQueue) of
                 {empty, WorkQueue} ->
-                    basis:timerstart(),
                     case queue:out(OtherAgents) of
                         {empty, OtherAgents} ->
                             Requester ! nothing_yet,
                             agent_loop(Worker, Master, WorkQueue, OtherAgents);
                         {{value, FirstAgent}, NewAgentQueue} ->
-                            case checkforwork(FirstAgent) of
-                                nothing_yet ->
-                                    basis:timerend(),
-                                    Requester ! nothing_yet;
-                                {work,_,_}=Work ->
-                                    io:format("Overhead is ~p~n", [basis:timerend()]),
-                                    Requester ! Work
-                            end,
+                            checkforwork(FirstAgent, Requester).
                             agent_loop(Worker, Master, WorkQueue,
                                        queue:in(FirstAgent, NewAgentQueue))
                    end;
