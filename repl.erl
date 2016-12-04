0ccf18e5f61a8214176019666771d1cfcbad6986
Maxwell Bernstein <tekknolagi@gmail.com>
2016-12-04 17:46:26 -0500

    Swap out lists:map for parallel_map in the hopes of speeding up startup

--- repl.erl
+++ repl.erl
@@ -11,13 +11,14 @@ main(Machines) when is_list(Machines) ->
     IdServer = spawn(fun master:idserver/0),
     Agents = master:connect_worker_nodes(Machines, flat, 2),
     AgentList = queue:to_list(Agents),
-    lists:map(fun(Agent) ->
-                      % Don't let the agent request work from itself.
-                      OtherAgents = AgentList -- [Agent],
-                      % And randomize the list so they don't all kill one node
-                      % at a time.
-                      Agent ! {other_agents, shuffle(OtherAgents)}
-              end, AgentList),
+    Map = fun basis:parallel_map/2,
+    Map(fun(Agent) ->
+                % Don't let the agent request work from itself.
+                OtherAgents = AgentList -- [Agent],
+                % And randomize the list so they don't all kill one node
+                % at a time.
+                Agent ! {other_agents, shuffle(OtherAgents)}
+        end, AgentList),
     StartingEnv = eval:bind('__idserver', IdServer,
                             eval:bind('__agents', Agents,
                                       basis:basis())),
